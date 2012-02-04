00902937314e488967420952bb8d2d95ff95d5e2
CMToups <CMToups@me.com>
2012-02-04 14:38:39 -0500

    starting my2app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+My2app::Application.config.secret_token = '14bcda51822fadc7f6482be7e197918ab29d6b0e8448606b6018cabedcdeaf750bbdd1035546c8df2d0d152a16cb329fe90499f6a425b57baabea32682aeee78'
