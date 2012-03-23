cbbc1a25efb7127216c4ca4b398263a64403e7f5
CMToups <CMToups@me.com>
2012-03-22 22:01:00 -0400

    set surve satic pages = true for porduction, not sure if this will help

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 My2app::Application.routes.draw do
+  
+  root :to => "home#index"
+  
   resources :characters
 
-  resources :ability
-
-  match '/view/:id' => 'ability#view'
+  resources :abilities
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +55,7 @@ My2app::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-    root :to => "home#index"
+  #  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
