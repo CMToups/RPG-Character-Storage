00b97355baf23d5fab25b5478c4d47ecfbfc308d
CMToups <CMToups@me.com>
2012-03-26 20:57:36 -0400

    Added an observer for ability that updates skill's mod and total.

--- app/models/notification_observer.rb
+++ app/models/notification_observer.rb
@@ -1,3 +1,10 @@
 class NotificationObserver < ActiveRecord::Observer
-  observe Ability
+  observe :ability
+  
+  def after_save(ability)
+    @skill = Skill.where(:character_id => ability.character_id)
+    @skill.each do |skill|
+      skill.save
+    end
+  end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,12 +3,23 @@ class Skill < ActiveRecord::Base
   
   belongs_to :Character
   belongs_to :Skill_type
-  has_one :Ability, :through => :Character
+  #has_one :Ability, :through => :Character
   
   validate before_create :init
-  validate before_save :set_mod, :set_total
+  validate before_save :update_mod, :set_total
   
   validates :skill_type_id, :character_id, :presence => true
+  
+  def update_mod
+      @st = SkillType.where(:id => self.skill_type_id).first
+      @a = Ability.where(:id => self.character_id).first 
+      @mod_key = @st.modifier_type
+      p @st
+      p @a
+      p @mod_key
+      mod_value_will_change!
+      self.mod_value = @a.send(@mod_key)
+  end
   
   private
   
@@ -22,16 +33,5 @@ class Skill < ActiveRecord::Base
     @value = (self.rank.to_i + self.misc.to_i + self.mod_value.to_i)
     total_will_change! if @value
     write_attribute(:total, @value )
-  end
-  
-  def set_mod
-      @st = SkillType.where(:id => self.skill_type_id).first
-      @a = self.Ability
-      @mod_key = @st.modifier_type
-      p @st
-      p @a
-      p @mod_key
-      mod_value_will_change!
-      self.mod_value = @a.send(@mod_key)
   end
 end
