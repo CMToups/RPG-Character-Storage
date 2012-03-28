5d8847d897026a019dd4e861cd70f2ad49e77e88
CMToups <CMToups@me.com>
2012-03-28 18:53:59 -0400

    fixed an error in skill types create migration

--- db/migrate/20120328215153_create_skill_types.rb
+++ db/migrate/20120328215153_create_skill_types.rb
@@ -2,6 +2,11 @@ class CreateSkillTypes < ActiveRecord::Migration
   def change
     create_table :skill_types do |t|
 
+      t.string :name
+      t.string :discription
+      t.string :modifier_type
+      t.boolean :default
+      
       t.timestamps
     end
   end
