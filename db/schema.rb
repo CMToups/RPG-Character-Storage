89ef8a430f997bb04546d89259b9a4e2c3dc2074
CMToups <CMToups@me.com>
2012-03-29 18:02:06 -0400

    added sort by name to characte show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120325220434) do
+ActiveRecord::Schema.define(:version => 20120328215153) do
 
   create_table "abilities", :force => true do |t|
     t.integer  "strength"
