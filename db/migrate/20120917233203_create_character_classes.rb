204d36f27cf0ed8417f318037b5325a3e72d190b
Conner <cmtoups@me.com>
2012-09-17 19:32:19 -0400

    Added character class.

---
+++ db/migrate/20120917233203_create_character_classes.rb
@@ -0,0 +1,11 @@
+class CreateCharacterClasses < ActiveRecord::Migration
+  def change
+    create_table :character_classes do |t|
+      t.integer :character_id
+      t.integer :character_class_type_id
+      t.integer :level
+
+      t.timestamps
+    end
+  end
+end
