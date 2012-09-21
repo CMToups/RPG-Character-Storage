f33f052da81f043a9eff3731ba01fa7c30998f65
Conner <cmtoups@me.com>
2012-09-20 20:48:07 -0400

    Made all relationships in effectable required, added spec.

--- app/models/effectable.rb
+++ app/models/effectable.rb
@@ -2,4 +2,7 @@ class Effectable < ActiveRecord::Base
 	belongs_to :effect
 	belongs_to :effectee, :polymorphic => true
 	belongs_to :character
+	
+	validates_presence_of :effect, :effectee, :character
+	
 end
--- spec/models/effectable_spec.rb
+++ spec/models/effectable_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Effectable do
+  
   it "Should belong to an effect" do 
   	should belong_to :effect
 	end
@@ -11,5 +12,17 @@ describe Effectable do
   
   it "should belong to a character" do 
   	should belong_to :character
+  end
+  
+  it "should not valid without an effect" do 
+  	Effectable.create(:effectee => Race.create, :character => Character.create(:name => :some_naem)).should_not be_valid
+  end
+  
+  it "should not valid without an effectee" do 
+  	Effectable.create(:effect => Effect.create, :character => Character.create(:name => :some_naem)).should_not be_valid
+  end
+  
+  it "should not valid without an character" do 
+  	Effectable.create(:effectee => Race.create, :effect => Effect.create).should_not be_valid
   end
 end
